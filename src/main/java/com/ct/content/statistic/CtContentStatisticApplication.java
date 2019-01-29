49b0dde3213ec218135abf6d973f1f1f6e92dfb0
zuodan <yudingling@126.com>
2019-01-29 18:42:00 +0800

    update

---
+++ src/main/java/com/ct/content/statistic/CtContentStatisticApplication.java
@@ -0,0 +1,14 @@
+package com.ct.content.statistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtContentStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtContentStatisticApplication.class, args);
+	}
+
+}
+
